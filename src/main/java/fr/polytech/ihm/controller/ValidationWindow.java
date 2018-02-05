5b25556dcd31801a3979379ff61e0ce1c87eb4d8
Jason Haenlin <jason.Haenlin@etu.unice.fr>
2018-02-05 17:25:45 +0100

    rajoute le bouton valider sur validationWindow. Ajuste la taille des fenêtre pour qu'elle soit de la même taille

--- src/main/java/fr/polytech/ihm/controller/ValidationWindow.java
+++ src/main/java/fr/polytech/ihm/controller/ValidationWindow.java
@@ -1,11 +1,18 @@
 package fr.polytech.ihm.controller;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
 
 public class ValidationWindow {
 
@@ -60,4 +67,24 @@ public class ValidationWindow {
     @FXML
     private Label validLabel;
 
+    @FXML
+    public void initialize() {
+        validButton.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+
+                String fxmlFile = "/fxml/freyja-homePage.fxml";
+                FXMLLoader loader = new FXMLLoader();
+                try {
+                    Stage stage = (Stage) validButton.getScene().getWindow();
+                    Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+
+                    Scene scene = new Scene(rootNode);
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
